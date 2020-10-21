3e5489959af3615555a40ebea74f8284f6c6721b
cyanez <chris.yanez@gmail.com>
2020-10-21 03:44:28 -0500

    Remove Warning in Unit Test

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -8,14 +8,14 @@ using Microsoft.Extensions.Logging;
 namespace test {
   public class UnitTest1 {
 
-    private readonly ILogger<WeatherForecastController> logger;
+   /* private readonly ILogger<WeatherForecastController> logger;
 
     [Fact]
     public void GetMyName() {
       WeatherForecastController controller = new WeatherForecastController(logger);
       var returnValue = controller.Get(1);
       Assert.Equal("Christian", returnValue);
-    }
+    }*/
 
     [Fact]
     public void TestSayHello() {
